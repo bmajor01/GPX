f1295d3e2d18f4d615aac05555a0e50f5872319f
Bela Major <36770843+bmajor01@users.noreply.github.com>
2024-05-08 16:22:07 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ import numpy as np
 from gpxplotter import read_gpx_file, create_folium_map, add_segment_to_map
 import folium
 from math import pi, sqrt, exp
+import matplotlib as mpl
+
+from matplotlib.collections import LineCollection
+from matplotlib.colors import BoundaryNorm, ListedColormap
 
 
 def gauss(n,sigma):
     r = range(-int(n/2),int(n/2)+1)
     return [1 / (sigma * sqrt(2*pi)) * exp(-float(x)**2/(2*sigma**2)) for x in r]
 
-kernel = gauss(10,2)
+#kernel = gauss(15,2)
+n = 2
+kernel = np.ones(n)
+kernel = kernel / n
 
 m = folium.Map(location=(47.1625, 19.5033),zoom_start=8)
 folium.TileLayer('openstreetmap', name='OpenStreet Map').add_to(m)
@@ -29,7 +36,7 @@ print("{} point(s)".format(len(segment.points)))
 
 print(track.length_3d())
 
-days = 3
+days = 1
 
 sliced = slicer.slice_gpx_at_interval(gpx, (track.length_3d()/days))
 
@@ -54,7 +61,7 @@ for n in sliced.tracks:
     m.save('KK_day'+str(fname)+'.html')
     import webbrowser
 
-    webbrowser.open('KK_day'+str(fname)+'.html')
+    #webbrowser.open('KK_day'+str(fname)+'.html')
 
     m = folium.Map(location=(47.1625, 19.5033),zoom_start=8)
 
@@ -71,7 +78,46 @@ for n in sliced.tracks:
         else:
             prevElevation = currentElevation
 
-    filt = np.convolve(x,kernel,mode='valid')
+    filt = np.convolve(x,kernel,mode='same')
+
+    filt = np.delete(filt, 0)
+
+    y = filt
+    x = np.arange(len(filt))
+    dydx = np.log10(abs(np.diff(y))+1)
+    dydx = dydx - np.min(dydx)
+    dydx = dydx / np.max(dydx)
+
+    plt.plot(dydx)
+    plt.show()
+
+    fig, ax = plt.subplots()
+
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+    norm = plt.Normalize(dydx.min(), dydx.max())
+    lc = LineCollection(segments, cmap='plasma', norm=norm)
+
+    lc.set_array(dydx)
+    lc.set_linewidth(2)
+    line = ax.add_collection(lc)
+    fig.colorbar(line)
+
+    ax.set_xlim(x.min(), x.max())
+    ax.set_ylim(y.min(), y.max())
+
+    npts = len(y)
+    colourmap = mpl.cm.get_cmap('plasma')
+    normalize = mpl.colors.Normalize(vmin=dydx.min(), vmax=dydx.max())
+
+    for i in range(npts - 1):
+        plt.fill_between([x[i], x[i + 1]],
+                         [y[i], y[i + 1]],
+                         color=colourmap(normalize(dydx[i]))
+                         , alpha=0.6)
+
+    plt.show()
 
     for points in filt:
         currentFilteredElevation = points
